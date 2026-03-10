71f1982cd4e57e924b6186d3d6b977b815d8d27c
Duong Do <duongdt011099@gmail.com>
2026-03-10 15:42:01 +0700

    format code and add readme

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using BingDailyWallpaper.Abstractions;
+using BingDailyWallpaper.Abstractions;
 using BingDailyWallpaper.Factories;
 using BingWallpaper.Services;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,72 +7,72 @@ using Microsoft.Extensions.Hosting;
 var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
-      services.AddHttpClient("Bing", config =>
-      {
-        config.BaseAddress = new Uri("https://www.bing.com");
-      }).RemoveAllLoggers();
-      services.AddTransient<IBingWallpaperDownloader, BingWallpaperDownloader>();
+        services.AddHttpClient("Bing", config =>
+        {
+            config.BaseAddress = new Uri("https://www.bing.com");
+        }).RemoveAllLoggers();
+        services.AddTransient<IBingWallpaperDownloader, BingWallpaperDownloader>();
     })
     .Build();
 
 if (args.Length == 0)
 {
-  Console.WriteLine("Unknown parameter: No command provided, please use 'bwall --help' to see available commands");
-  return;
+    Console.WriteLine("Unknown parameter: No command provided, please use 'bwall --help' to see available commands");
+    return;
 }
 
 var service = host.Services.GetRequiredService<IBingWallpaperDownloader>();
 
 async Task HandleDownload(int idx = 0)
 {
-  var localPath = await service.DownloadWallpaperAsync(idx);
-  if (localPath != null)
-  {
-    var desktop =
-            Environment.GetEnvironmentVariable("XDG_CURRENT_DESKTOP") ??
-            Environment.GetEnvironmentVariable("XDG_SESSION_DESKTOP") ??
-            Environment.GetEnvironmentVariable("DESKTOP_SESSION");
-
-    if (string.IsNullOrEmpty(desktop))
+    var localPath = await service.DownloadWallpaperAsync(idx);
+    if (localPath != null)
     {
-      Console.WriteLine("Unable to determine desktop environment, defaulting to GNOME.");
-      desktop = "gnome";
-    }
+        var desktop =
+                Environment.GetEnvironmentVariable("XDG_CURRENT_DESKTOP") ??
+                Environment.GetEnvironmentVariable("XDG_SESSION_DESKTOP") ??
+                Environment.GetEnvironmentVariable("DESKTOP_SESSION");
 
-    var desktopEnvironment = DesktopEnvironmentFactory.Create(desktop);
-    desktopEnvironment.SetWallpaper(localPath);
-  }
-  else
-  {
-    Console.WriteLine("Failed to download wallpaper.");
-  }
+        if (string.IsNullOrEmpty(desktop))
+        {
+            Console.WriteLine("Unable to determine desktop environment, defaulting to GNOME.");
+            desktop = "gnome";
+        }
+
+        var desktopEnvironment = DesktopEnvironmentFactory.Create(desktop);
+        desktopEnvironment.SetWallpaper(localPath);
+    }
+    else
+    {
+        Console.WriteLine("Failed to download wallpaper.");
+    }
 }
 
 switch (args[0])
 {
-  case "today":
-    await HandleDownload(0);
-    break;
+    case "today":
+        await HandleDownload(0);
+        break;
 
-  case "previous":
-    if (args.Length > 1 && int.TryParse(args[1], out int previousDays))
-    {
-      await HandleDownload(previousDays);
-    }
-    else
-    {
-      await HandleDownload(1);
-    }
-    break;
+    case "previous":
+        if (args.Length > 1 && int.TryParse(args[1], out int previousDays))
+        {
+            await HandleDownload(previousDays);
+        }
+        else
+        {
+            await HandleDownload(1);
+        }
+        break;
 
-  case "--help":
-    Console.WriteLine("Available commands:");
-    Console.WriteLine("  today     - Download today's wallpaper");
-    Console.WriteLine("  previous <number> - Download the previous wallpaper, <number> is how many previous days to go back (default is 0)");
-    Console.WriteLine("  --help    - Show this help message");
-    break;
+    case "--help":
+        Console.WriteLine("Available commands:");
+        Console.WriteLine("  today     - Download today's wallpaper");
+        Console.WriteLine("  previous <number> - Download the previous wallpaper, <number> is how many previous days to go back (default is 0)");
+        Console.WriteLine("  --help    - Show this help message");
+        break;
 
-  default:
-    Console.WriteLine("Unknown command, please use 'bwall --help' to see available commands");
-    break;
+    default:
+        Console.WriteLine("Unknown command, please use 'bwall --help' to see available commands");
+        break;
 }
--- Services/BingWallpaperDownloader.cs
+++ Services/BingWallpaperDownloader.cs
@@ -6,50 +6,50 @@ namespace BingWallpaper.Services;
 
 public class BingWallpaperDownloader : IBingWallpaperDownloader
 {
-  private const string BingWallpaperEndpoint = "/HPImageArchive.aspx?format=js&idx={0}&n=1&mkt=en-US";
-  private readonly IHttpClientFactory _httpClientFactory;
+    private const string BingWallpaperEndpoint = "/HPImageArchive.aspx?format=js&idx={0}&n=1&mkt=en-US";
+    private readonly IHttpClientFactory _httpClientFactory;
 
-  public BingWallpaperDownloader(IHttpClientFactory httpClientFactory)
-  {
-    _httpClientFactory = httpClientFactory;
-  }
-
-  public async Task<string?> DownloadWallpaperAsync(int idx = 0)
-  {
-    var httpClient = _httpClientFactory.CreateClient("Bing");
-    var response = await httpClient.GetAsync(string.Format(BingWallpaperEndpoint, idx));
-    response.EnsureSuccessStatusCode();
-
-    var content = await response.Content.ReadAsStringAsync();
-    var bingWallpaperResponse = JsonSerializer.Deserialize<BingResponse>(content);
-
-    if (bingWallpaperResponse == null || bingWallpaperResponse?.Images == null)
+    public BingWallpaperDownloader(IHttpClientFactory httpClientFactory)
     {
-      Console.WriteLine("Failed to parse Bing wallpaper response.");
-      return null;
+        _httpClientFactory = httpClientFactory;
     }
 
-    var image = bingWallpaperResponse.Images[0];
-    var wallpaperUrl = $"https://www.bing.com{image.Url}";
-    var wallpaperResponse = await httpClient.GetAsync(wallpaperUrl);
-    wallpaperResponse.EnsureSuccessStatusCode();
-
-    var home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);;
-    var directory = $"{home}/Pictures/Bing Wallpapers";
-
-    if (!Directory.Exists(directory))
+    public async Task<string?> DownloadWallpaperAsync(int idx = 0)
     {
-      Directory.CreateDirectory(directory);
+        var httpClient = _httpClientFactory.CreateClient("Bing");
+        var response = await httpClient.GetAsync(string.Format(BingWallpaperEndpoint, idx));
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+        var bingWallpaperResponse = JsonSerializer.Deserialize<BingResponse>(content);
+
+        if (bingWallpaperResponse == null || bingWallpaperResponse?.Images == null)
+        {
+            Console.WriteLine("Failed to parse Bing wallpaper response.");
+            return null;
+        }
+
+        var image = bingWallpaperResponse.Images[0];
+        var wallpaperUrl = $"https://www.bing.com{image.Url}";
+        var wallpaperResponse = await httpClient.GetAsync(wallpaperUrl);
+        wallpaperResponse.EnsureSuccessStatusCode();
+
+        var home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile); ;
+        var directory = $"{home}/Pictures/Bing Wallpapers";
+
+        if (!Directory.Exists(directory))
+        {
+            Directory.CreateDirectory(directory);
+        }
+
+        var filePath = $"{directory}/{image.FullStartDate}.jpg";
+
+        if (!File.Exists(filePath))
+        {
+            await using var fs = new FileStream(filePath, FileMode.Create);
+            await wallpaperResponse.Content.CopyToAsync(fs);
+        }
+
+        return filePath;
     }
-
-    var filePath = $"{directory}/{image.FullStartDate}.jpg";
-
-    if (!File.Exists(filePath))
-    {
-      await using var fs = new FileStream(filePath, FileMode.Create);
-      await wallpaperResponse.Content.CopyToAsync(fs);
-    }
-
-    return filePath;
-  }
 }
